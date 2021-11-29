de421478e9e53d2ee538cb787e7568576fd09004
Countern <57810363+Countern@users.noreply.github.com>
2021-11-28 18:22:05 -0600

    Removed port variables

--- index.js
+++ index.js
@@ -2,15 +2,9 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
-const port = 3000;
-
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
     //res.send("Hello, World!");
     res.sendFile(path.join(__dirname, "public", "home.html"));
-});
-
-app.listen(port, ()=> {
-    console.log(`Web is running on Port: ${port}`)
 });
